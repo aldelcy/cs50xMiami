aebee3111e4453e8ae64cb01463c665edcc950d6
Al Delcy <aldelcy@users.noreply.github.com>
2021-03-16 18:01:26 -0400

    Update psets.py

--- cs50x/data/psets.py
+++ cs50x/data/psets.py
@@ -6,7 +6,7 @@ psets = {
     "desc" : """
       <ol>
         <li>Download and install the latest version of <a href="https://www.google.com/chrome/">Chrome</a>, if you don’t have it already.</li>
-        <li>Implement your very own Scratch project using Chrome, per <a href="scratch">this specification</a>.</li>
+        <li>Implement your very own Scratch project using Chrome, per <a href="https://cs50.harvard.edu/x/2021/psets/0/scratch">this specification</a>.</li>
       </ol>
     """,
     "link" : "https://cs50.harvard.edu/x/2021/psets/0/",
@@ -16,43 +16,40 @@ psets = {
     "id": 1,
     "desc" : """
       <ol>
-        <li>Reply to <a href="https://us.edstem.org/courses/175/discussion/14881">this thread</a> to say hello (and, if you’d like, a little about yourself!) to other students and staff.</li>
-        <li>Submit <a href="https://lab.cs50.io/cs50/labs/2020/spring/hello/">Hello</a></li>
+        <li>Submit <a href="https://cs50.harvard.edu/x/2021/labs/1/hello/">Hello</a></li>
         <li>Submit one of:
           <ul class="fa-ul">
-            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="https://lab.cs50.io/cs50/labs/2020/spring/mario/less/">this version of Mario</a> if feeling less comfortable</li>
-            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="https://lab.cs50.io/cs50/labs/2020/spring/mario/more/">this version of Mario</a> if feeling more comfortable</li>
+            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="https://cs50.harvard.edu/x/2021/psets/1/mario/less/">this version of Mario</a> if feeling less comfortable</li>
+            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="https://cs50.harvard.edu/x/2021/psets/1/mario/more/">this version of Mario</a> if feeling more comfortable</li>
           </ul>
         </li>
         <li>Submit one of:
           <ul class="fa-ul">
-            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="https://lab.cs50.io/cs50/labs/2020/spring/cash/">Cash</a> if feeling less comfortable</li>
-            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="https://lab.cs50.io/cs50/labs/2020/spring/credit/">Credit</a> if feeling more comfortable</li>
+            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="https://cs50.harvard.edu/x/2021/psets/1/cash/">Cash</a> if feeling less comfortable</li>
+            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="https://cs50.harvard.edu/x/2021/psets/1/credit/">Credit</a> if feeling more comfortable</li>
           </ul>
         </li>
-        <li>Submit <a href="https://forms.cs50.io/d1b7f746-47c4-4930-b9bf-d6e6a92e2528">this form</a>!</li>
       </ol>
     """,
     "link" : "https://cs50.harvard.edu/x/2021/psets/1/",
-    "due_date" : date(2021, 3, 13)
+    "due_date" : date(2021, 3, 20)
   },
   2: {
     "id": 2,
     "desc" : """
       <ol>
         <li>Go to <a href="https://ide.cs50.io">ide.cs50.io</a> and click “Sign in with GitHub” to access your CS50 IDE.</li>
-        <li>Submit <a href="readability">Readability</a></li>
+        <li>Submit <a href="https://cs50.harvard.edu/x/2021/psets/2/readability">Readability</a></li>
         <li>Submit one of:
           <ul class="fa-ul">
-            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="caesar">Caesar</a> if feeling less comfortable</li>
-            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="substitution">Substitution</a> if feeling more comfortable</li>
+            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="https://cs50.harvard.edu/x/2021/psets/2/caesar">Caesar</a> if feeling less comfortable</li>
+            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="https://cs50.harvard.edu/x/2021/psets/2/substitution">Substitution</a> if feeling more comfortable</li>
           </ul>
         </li>
-        <li>Submit <a href="https://forms.cs50.io/9fe04482-eda8-4eed-b501-a7a7978e94b5">this form</a></li>
       </ol>
     """,
     "link" : "https://cs50.harvard.edu/x/2021/psets/2/",
-    "due_date" : date(2020, 8, 22)
+    "due_date" : date(2020, 3, 27)
   },
   3: {
     "id": 3,
@@ -69,7 +66,7 @@ psets = {
       </ol>
     """,
     "link" : "https://cs50.harvard.edu/x/2021/psets/3/",
-    "due_date" : date(2020, 3, 8)
+    "due_date" : date(2020, 4, 3)
   },
   4: {
     "id": 4,
@@ -86,22 +83,17 @@ psets = {
       </ol>
     """,
     "link" : "https://cs50.harvard.edu/x/2021/psets/4/",
-    "due_date" : date(2020, 3, 22)
+    "due_date" : date(2020, 4, 10)
   },
   5: {
     "id": 5,
     "desc" : """
       <ol>
-        <li>Submit <a href="speller">Speller</a></li>
-        <li>Submit <a href="https://forms.cs50.io/486e6ddb-6267-4a59-a94f-76ced30cc65f">this form</a>
-          <ul class="fa-ul">
-            <li data-marker="-"><span class="fa-li"><i class="far fa-minus-square"></i></span>Note: This form is a bit longer than most weeks, so be sure to leave yourself enough time!</li>
-          </ul>
-        </li>
+        <li>Submit <a href="https://cs50.harvard.edu/x/2021/psets/5/speller">Speller</a></li>
       </ol>
     """,
     "link" : "https://cs50.harvard.edu/x/2021/psets/5/",
-    "due_date" : date(2020, 4, 5)
+    "due_date" : date(2020, 4, 17)
   },
   6: {
     "id": 6,
@@ -110,134 +102,53 @@ psets = {
         <li>Submit <a href="hello">Hello</a> in Python</li>
         <li>Submit one of:
           <ul class="fa-ul">
-            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="mario/less">this version of Mario</a> in Python, if feeling less comfortable</li>
-            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="mario/more">this version of Mario</a> in Python, if feeling more comfortable</li>
+            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="https://cs50.harvard.edu/x/2021/psets/6/mario/less">this version of Mario</a> in Python, if feeling less comfortable</li>
+            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="https://cs50.harvard.edu/x/2021/psets/6/mario/more">this version of Mario</a> in Python, if feeling more comfortable</li>
           </ul>
         </li>
         <li>Submit one of:
           <ul class="fa-ul">
-            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="cash">Cash</a> in Python, if feeling less comfortable</li>
-            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="credit">Credit</a> in Python, if feeling more comfortable</li>
+            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="https://cs50.harvard.edu/x/2021/psets/6/cash">Cash</a> in Python, if feeling less comfortable</li>
+            <li data-marker="*"><span class="fa-li"><i class="fas fa-circle"></i></span><a href="https://cs50.harvard.edu/x/2021/psets/6/credit">Credit</a> in Python, if feeling more comfortable</li>
           </ul>
         </li>
-        <li>Submit <a href="readability">Readability</a> in Python</li>
-        <li>Submit <a href="dna">DNA</a> in Python</li>
-        <li>Submit <a href="https://forms.cs50.io/34c2a163-f85f-47fc-8897-bba1ead85af1">this form</a></li>
+        <li>Submit <a href="https://cs50.harvard.edu/x/2021/psets/6/readability">Readability</a> in Python</li>
+        <li>Submit <a href="https://cs50.harvard.edu/x/2021/psets/6/dna">DNA</a> in Python</li>
       </ol>
     """,
     "link" : "https://cs50.harvard.edu/x/2021/psets/6/",
-    "due_date" : date(2020, 4, 19)
+    "due_date" : date(2020, 4, 24)
   },
   7: {
     "id": 7,
     "desc" : """
     <ol>
-      <li>Submit <a href="movies">Movies</a></li>
-      <li>Submit <a href="houses">Houses</a></li>
-      <li>Submit <a href="https://forms.cs50.io/d3f3ced8-0e2c-4f83-afc4-6d10d469508a">this form</a></li>
+      <li>Submit <a href="https://cs50.harvard.edu/x/2021/psets/7/movies/">Movies</a></li>
+      <li>Submit <a href="https://cs50.harvard.edu/x/2021/psets/7/fiftyville/">Houses</a></li>
     </ol>
     """,
     "link" : "https://cs50.harvard.edu/x/2021/psets/7/",
-    "due_date" : date(2020, 5, 3)
+    "due_date" : date(2020, 5, 8)
   },
   8: {
     "id": 8,
-    "desc" : """ Coming soon """,
-    "link" : "",
-    "due_date" : date(2020, 5, 17)
+    "desc" : """ Build a simple homepage using HTML, CSS, and JavaScript. 
+    <ol>
+      <li>Submit <a href="https://cs50.harvard.edu/x/2021/psets/8/homepage/">HomePage</a></li>
+    </ol>""",
+    "link" : "https://cs50.harvard.edu/x/2021/psets/8/",
+    "due_date" : date(2020, 5, 22)
+  },
+  9: {
+    "id": 9,
+    "desc" : """
+    Implement a website via which users can “buy” and “sell” stocks, a la the below.
+    <ol>
+      <li>Submit <a href="https://cs50.harvard.edu/x/2021/psets/9/finance/">Finance</a></li>
+    </ol>
+    """,
+    "link" : "https://cs50.harvard.edu/x/2021/psets/9/",
+    "due_date" : date(2020, 6, 7)
   }
 }
 
-
-
-
-# psets2019 = {
-#   0: {
-#     "desc" : "<p>Implement your very own Scratch project using Chrome, per <a href='https://docs.cs50.net/2019/x/psets/0/scratch.html'>this specification</a>.</p>",
-#     "link" : "https://docs.cs50.net/2019/x/psets/0/index.html",
-#     "due_date" : date(2019, 1, 29)
-#   },
-#   1: {
-#     "desc" : """<p>Submit <a href='https://lab.cs50.io/cs50/labs/2019/x/hello/'>Hello</a></p>
-#             <p>Submit one of:</p>
-#             <ul>
-#                 <li><a href='https://lab.cs50.io/cs50/labs/2019/x/mario/less/'>this version of Mario</a> if feeling less comfortable</li>
-#                 <li><a href='https://lab.cs50.io/cs50/labs/2019/x/mario/more/'>this version of Mario</a> if feeling more comfortable</li>
-#             </ul>
-#             <p>Submit one of:</p>
-#             <ul>
-#                 <li><a href='https://lab.cs50.io/cs50/labs/2019/x/cash/'>Cash</a> if feeling less comfortable</li>
-#                 <li><a href='https://lab.cs50.io/cs50/labs/2019/x/credit/'>Credit</a> if feeling more comfortable</li>
-#             </ul>""",
-#     "link" : "https://docs.cs50.net/2019/x/psets/1/index.html",
-#     "due_date" : date(2019, 2, 16)
-#   },
-#   2: {
-#     "desc" : """<p>Submit any two (2) of:</p>\
-#             <ul>\
-#                 <li><a href='https://lab.cs50.io/cs50/labs/2019/x/caesar/'>Caesar</a>, if feeling less comfortable</li>
-#                 <li><a href='https://lab.cs50.io/cs50/labs/2019/x/vigenere/'>Vigen&egrave;re</a>, if feeling less comfortable</li>
-#                 <li><a href='https://lab.cs50.io/cs50/labs/2019/x/crack/'>Crack</a>, if feeling more comfortable</li>
-#             </ul>""",
-#     "link" : "https://docs.cs50.net/2019/x/psets/2/index.html",
-#     "due_date" : date(2019, 2, 23)
-#   },
-#   3: {
-#     "desc" : """<p>Submit, using CS50 Lab, <a href='https://lab.cs50.io/cs50/labs/2019/x/whodunit/'>Whodunit</a></p>
-#             <p>Submit, using CS50 IDE, one of:</p>
-#             <ul>
-#                 <li><a href='https://docs.cs50.net/2019/x/psets/3/resize/less/resize.html'>this version of Resize</a>, if feeling less comfortable</li>
-#                 <li><a href='https://docs.cs50.net/2019/x/psets/3/resize/more/resize.html'>this version of Resize</a>, if feeling more comfortable</li>
-#             </ul>
-#             <p>Submit <a href='https://docs.cs50.net/2019/x/psets/3/recover/recover.html'>Recover</a>, using CS50 IDE</p>""",
-#     "link" : "https://docs.cs50.net/2019/x/psets/3/index.html",
-#     "due_date" : date(2019, 3, 8)
-#   },
-#   4: {
-#     "desc" : """<p>Submit, using CS50 IDE, one of:</p>
-#             <ul>
-#                 <li><a href='https://docs.cs50.net/2019/x/psets/4/speller/hashtable/speller.html'>this version of Speller</a>, if you&rsquo;d prefer to implement a hash table</li>
-#                 <li><a href='https://docs.cs50.net/2019/x/psets/4/speller/trie/speller.html'>this version of Speller</a>, if you&rsquo;d prefer to implement a trie</li>
-#             </ul>""",
-#     "link" : "https://docs.cs50.net/2019/x/psets/4/index.html",
-#     "due_date" : date(2019, 3, 22)
-#   },
-#   5: {
-#     "desc" : "<p>Submit, using CS50 IDE, <a href='https://docs.cs50.net/2019/x/psets/5/homepage/homepage.html'>Homepage</a></p>",
-#     "link" : "https://docs.cs50.net/2019/x/psets/5/index.html",
-#     "due_date" : date(2019, 4, 5)
-#   },
-#   6: {
-#     "desc" : """<p>Submit, using CS50 IDE, <a href='https://docs.cs50.net/2019/x/psets/6/sentimental/hello/hello.html'>Hello</a> in Python</p>
-#             <p>Submit, using CS50 IDE, one of:</p>
-#             <ul>
-#                 <li><a href='https://docs.cs50.net/2019/x/psets/6/sentimental/mario/less/mario.html'>this version of Mario</a> in Python, if feeling less comfortable</li>
-#                 <li><a href='https://docs.cs50.net/2019/x/psets/6/sentimental/mario/more/mario.html'>this version of Mario</a> in Python, if feeling more comfortable</li>
-#             </ul>
-#             <p>Submit, using CS50 IDE, one of:</p>
-#             <ul>
-#                 <li><a href='https://docs.cs50.net/2019/x/psets/6/sentimental/cash/cash.html'>Cash</a> in Python</li>
-#                 <li><a href='https://docs.cs50.net/2019/x/psets/6/sentimental/credit/credit.html'>Credit</a> in Python</li>
-#             </ul>
-#             <p>Submit, using CS50 IDE, one of:</p>
-#             <ul>
-#                 <li><a href='https://docs.cs50.net/2019/x/psets/6/sentimental/caesar/caesar.html'>Caesar</a> in Python</li>
-#                 <li><a href='https://docs.cs50.net/2019/x/psets/6/sentimental/crack/crack.html'>Crack</a> in Python</li>
-#                 <li><a href='https://docs.cs50.net/2019/x/psets/6/sentimental/vigenere/vigenere.html'>Vigen&egrave;re</a> in Python</li>
-#             </ul>
-#             <p>Submit, using CS50 IDE, <a href='https://docs.cs50.net/2019/x/psets/6/bleep/bleep.html'>Bleep</a> in Python</p>""",
-#     "link" : "https://docs.cs50.net/2019/x/psets/6/index.html",
-#     "due_date" : date(2019, 4, 19)
-#   },
-#   7: {
-#     "desc" : """<p>Submit, using CS50 IDE, <a href='https://docs.cs50.net/2019/x/psets/7/similarities/similarities.html'>Similarities</a></p>
-#             <p>Submit, using CS50 IDE, <a href='https://docs.cs50.net/2019/x/psets/7/survey/survey.html'>Survey</a></p>""",
-#     "link" : "https://docs.cs50.net/2019/x/psets/7/index.html",
-#     "due_date" : date(2019, 5, 3)
-#   },
-#   8: {
-#     "desc" : """<p>Submit, using CS50 IDE, <a href='https://docs.cs50.net/2019/x/psets/8/finance/finance.html'>Finance</a></p>""",
-#     "link" : "https://docs.cs50.net/2019/x/psets/8/index.html",
-#     "due_date" : date(2019, 5, 17)
-#   }
-# }
